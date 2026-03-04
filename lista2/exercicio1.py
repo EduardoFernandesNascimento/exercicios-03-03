fb310e6ff46612d99f4eaad475576b67f317da68
EduardoFernandesNascimento <dudufernasc6789@gmail.com>
2026-03-03 23:10:29 -0300

    Update exercicio1.py

--- lista2/exercicio1.py
+++ lista2/exercicio1.py
@@ -18,3 +18,34 @@ print(f'A lista números contém: {len(numeros)} itens')
 
 # exercicio_4
 
+def verificaçao(n):
+    if n in numeros:
+        return 'está na lista'
+    else:
+        return 'nao está na lista'
+    
+print(verificaçao(7))
+
+# exercicio_5
+
+print(numeros[0:3])
+
+# exercicio_6
+
+notas = [8.5, 7.0, 9.5, 6.0]
+notas.sort(reverse=True)
+print(notas)
+
+# exercicio 7
+
+a=0
+
+for i in  numeros:
+    if i == 5:
+        a+=1
+
+print(f'o numero 5 aparece {a} vez(s) na lista numeros')
+
+# exercicio 8
+
+print(numeros[::-1])
